36085bfed6ae2f32d5b67cce78b7cfc8f95f6a06
huyz <huyz@users.noreply.github.com>
2022-08-01 06:10:32 -0700

    ✨ Automatically determine vault name

--- extension.js
+++ extension.js
@@ -1,9 +1,40 @@
 const vscode = require("vscode");
+const fs = require("fs");
+
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	const isWindows = "win32" === process.platform;
+
+	// Code borrowed from https://github.com/wraith13/open-in-github-desktop-vscode/blob/master/source/extension.ts
+	const fileExists = path => new Promise (
+		resolve => fs.exists(
+			path,
+			exists => resolve(exists)
+		)
+	);
+	const normalizePath = path => path.replace(isWindows ? /\\$/: /\/$/, "");
+	const isRootDir = path => isWindows ?
+        /^\w+\:$/.test(normalizePath(path)) ||
+        /^\\\\[^\\]+\\[^\\]+$/.test(normalizePath(path))
+    : "" === normalizePath(path);
+	const getParentDir = path => normalizePath(path).replace(isWindows ? /\\[^\\]+$/: /\/[^\/]+$/, "");
+
+	const searchObsidianConfig = async path =>
+	{
+		const obsConfigPath = `${path}/.obsidian`;
+		if (await fileExists(obsConfigPath)) {
+			return path;
+		}
+		if (!isRootDir(path)) {
+			return await searchObsidianConfig(getParentDir(path));
+		}
+		return null;
+	};
+
+
 	/**
 	 * @description 获取当前编辑器中文件的绝对路径以及文件标题
 	 */
@@ -22,27 +53,44 @@ function activate(context) {
 	/**
 	 * @description 获取 vault 名
 	 */
-	function getVaultName() {
+	async function getVaultName(path) {
+		const obsConfigPath = await searchObsidianConfig(getParentDir(path), true);
+		if (obsConfigPath) {
+			return obsConfigPath.replace(isWindows ? /.*\\([^\\]+)\\?/ : /.*\/([^\/]+)\/?/, '$1');
+		} 
+
 		return vscode.workspace
 			.getConfiguration("obsidian-code")
 			.get("vaultName");
 	}
 
 	// debug
-	console.log(`current vault: ${getVaultName()}`);
+	console.log(`obsidian-code: default vault: ${getVaultName()}`);
 
 	let disposable = vscode.commands.registerCommand(
 		"obsidian-code.openObsidian",
-		function () {
-			const { title } = getFileInfo();
-			const vaultName = getVaultName();
+		async function () {
+			// Check if a text editor is active
+			if (!vscode.window.activeTextEditor) {
+				console.error("obsidian-code: no text editor is active.");
+				return;
+			}
+			// Check if the file has been deleted recently
+			const scheme = vscode.window.activeTextEditor.document.uri.scheme;
+			if (scheme !== "file") {
+				vscode.window.showErrorMessage("Cannot find file. Has it been deleted?");
+				return;
+			}
+
+			const { path, title } = getFileInfo();
+			const vaultName = await getVaultName(path);
 			if (vaultName) {
 				// 根据 vault 名和文件名构建 URI
 				let obURI = `obsidian://open?vault=${vaultName}&file=${title}`;
 				vscode.env.openExternal(vscode.Uri.parse(obURI));
-				vscode.window.showInformationMessage(`opening file ${title}`);
+				vscode.window.showInformationMessage(`Opening file ${title} in Obsidian`);
 			} else {
-				vscode.window.showErrorMessage("please specify a vault name");
+				vscode.window.showErrorMessage("Please set a default vault name in the extension settings");
 			}
 		}
 	);
